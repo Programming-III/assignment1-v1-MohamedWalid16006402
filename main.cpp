998b9a4517b8caa27469594a9608f5a7ce008474
MohamedWalid16006402 <mohamed.walidibrahim@student.giu-uni.de>
2025-11-12 15:17:07 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,16 +1,52 @@
+#include <iostream>
+#include <string>
 #include "Animal.h"
 #include "Bird.h"
 #include "Mammal.h"
 #include "Reptile.h"
 #include "Enclosure.h"
 #include "Visitor.h"
-#include <iostream>
+
 using namespace std;
 
+class Main {
+public:
+    Main() {
+        
+        Animal* lion = new Mammal("lion", 5, true, "yellow");
+        Animal* parrot = new Bird("parrot", 2, false, 1.5f);
+        Animal* snake = new Reptile("snake", 3, true, true);
 
-// ============== MAIN FUNCTION ==============
+        /
+        Enclosure* enclosure = new Enclosure(3);  
+        enclosure->addAnimal(lion);
+        enclosure->addAnimal(parrot);
+        enclosure->addAnimal(snake);
+
+        
+        cout << "Enclosure 1 Animals:" << endl;
+        enclosure->displayAnimals();
+        
+        
+        Visitor* visitor = new Visitor("Sara Ali", 3);
+
+        
+        cout << "Visitor info:" << endl;
+        visitor->displayInfo();
+
+        
+        delete enclosure;  
+        delete visitor;
+    }
+
+    virtual ~Main() {}
+
+protected:
+
+private:
+};
+
 int main() {
-    
-    
+    Main app;
     return 0;
 }
