6c7ff96617ce7f7487352c153ec754b5606797fd
MohamedWalid16006402 <mohamed.walidibrahim@student.giu-uni.de>
2025-11-12 15:06:42 +0200

    Update Bird.h

--- Bird.h
+++ Bird.h
@@ -3,8 +3,19 @@
 
 #include "Animal.h"
 
-//define class bird here 
+class Bird : public Animal {
+private:
+    float wingSpan;
 
+public:
+    Bird(string name, int age, bool isHungry, float wingSpan)
+        : Animal(name, age, isHungry), wingSpan(wingSpan) {}
+
+    void display() {
+        Animal::display();
+        cout << "Wing Span: " << wingSpan << " meters" << endl;
+    }
+};
 
 
 
