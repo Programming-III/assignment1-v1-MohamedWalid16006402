672ed7a4ed2d1a0d43cfb55c6adea1e40656d8ed
MohamedWalid16006402 <mohamed.walidibrahim@student.giu-uni.de>
2025-11-12 15:08:45 +0200

    Update Reptile.h

--- Reptile.h
+++ Reptile.h
@@ -3,9 +3,22 @@
 
 #include "Animal.h"
 
-//define reptile class here
+class Reptile : public Animal {
+private:
+    bool isVenomous;
 
+public:
+    Reptile(string name, int age, bool isHungry, bool isVenomous)
+        : Animal(name, age, isHungry), isVenomous(isVenomous) 
+{
+          
+        }
 
+    void display() {
+        Animal::display();
+        cout << "Venomous: " << (isVenomous ? "Yes" : "No") << endl;
+    }
+};
 
 
 
