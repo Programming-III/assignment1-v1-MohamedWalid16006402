ac3787d4fb8aa46d22154f0e5a83a3b850084895
MohamedWalid16006402 <mohamed.walidibrahim@student.giu-uni.de>
2025-11-12 15:07:35 +0200

    Update Mammal.h

--- Mammal.h
+++ Mammal.h
@@ -3,7 +3,19 @@
 
 #include "Animal.h"
 
-//define mammal class here
+class Mammal : public Animal {
+private:
+    string furColor;
+
+public:
+    Mammal(string name, int age, bool isHungry, string furColor)
+        : Animal(name, age, isHungry), furColor(furColor) {}
+
+    void display() {
+        Animal::display();
+        cout << "Fur Color: " << furColor << endl;
+    }
+};
 
 
 
