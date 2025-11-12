5cccf5fc7a1371495fbc152f7f074eb5a54a852b
MohamedWalid16006402 <mohamed.walidibrahim@student.giu-uni.de>
2025-11-12 15:13:01 +0200

    Update Visitor.h

--- Visitor.h
+++ Visitor.h
@@ -1,15 +1,40 @@
+
 #ifndef VISITOR_H
 #define VISITOR_H
 
-#include <string>
+#include <iostream>
+#include <string> 
+
 using namespace std;
 
-//define visitor class here
+class Visitor {
+public:
+   
+    Visitor(string name = "Unknown", int tickets = 0) 
+        : vistorName(name), ticketsBought(tickets) {}
+
+    
+    virtual ~Visitor() {}
+
+    
+    void displayInfo() const {
+        cout << "Visitor Name: " << vistorName << endl;
+        cout << "Tickets Bought: " << ticketsBought << endl;
+    }
+
+    
+    string getVistorName() const { return vistorName; }
+    void setVistorName(const string& name) { vistorName = name; }
+
+    int getTicketsBought() const { return ticketsBought; }
+    void setTicketsBought(int tickets) { ticketsBought = tickets; }
+
+protected:
+    string vistorName;
+    int ticketsBought;
+};
+
+#endif 
 
 
 
-
-
-
-
-#endif
