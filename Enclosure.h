628b9017467df4e3761dc500bc4bbd294e912e65
MohamedWalid16006402 <mohamed.walidibrahim@student.giu-uni.de>
2025-11-12 15:10:05 +0200

    Update Enclosure.h

--- Enclosure.h
+++ Enclosure.h
@@ -3,13 +3,47 @@
 
 #include "Animal.h"
 
-//define enclosure class here 
+class Enclosure {
+public:
+    
+    Enclosure(int capacity) : capacity(capacity), currentCount(0) {
+        animals = new Animal*[capacity];  
+    }
 
+    
+    virtual ~Enclosure() {
+        for (int i = 0; i < currentCount; ++i) {
+            delete animals[i];  
+        }
+        delete[] animals;  
+    }
 
+    
+    void addAnimal(Animal* a) {
+        if (currentCount < capacity) {
+            animals[currentCount++] = a;  
+        } else {
+            cout << "Enclosure is full, cannot add more animals." << endl;
+        }
+    }
 
+    
+    void displayAnimals() {
+        if (currentCount == 0) {
+            cout << "No animals in the enclosure." << endl;
+        } else {
+            for (int i = 0; i < currentCount; ++i) {
+                animals[i]->display();  
+            }
+        }
+    }
 
+protected:
+    Animal** animals;  
 
+private:
+    int currentCount; 
+    int capacity;      
+};
 
-
-
-#endif
+#endif 
